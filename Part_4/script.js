7d076bcccf2c2dbbf672d528b86d4f5e7cd509dd
KevinChiputra <kevin.chiputra@binus.ac.id>
2024-02-27 14:44:47 +0700

    materi part 4 dan 5

---
+++ Part_4/script.js
@@ -0,0 +1,41 @@
+// function init(){
+//     let nama = "kevin"
+//     return function tampilNama(nama){
+//         console.log(nama)
+//     }
+
+// }
+
+// let panggilNama = init()
+// panggilNama('chi')
+
+
+// ==================================
+
+// function ucapkanSalam(waktu){
+//     return function(nama){
+//         console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan`)
+//     }
+// }
+
+// let selamatPagi = ucapkanSalam('pagi')
+// let selamatSiang = ucapkanSalam('siang')
+// let selamatMalam = ucapkanSalam('malam')
+
+// selamatPagi('kevin')
+
+
+// ========================
+
+let add = (function () {
+    let counter = 0
+    return function (){
+        return ++counter
+    }
+})()
+
+
+// let a = add()
+console.dir(add())
+console.dir(add())
+console.dir(add())
